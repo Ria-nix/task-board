dbfc54668124544980244e1e494baa5fdc1480c3
RiaNix <podkovadasha@gmail.com>
2026-01-19 16:45:32 +0200

    show the list of users from API

---
+++ main.js
@@ -0,0 +1,21 @@
+const url = "https://dummyjson.com/users";
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let users = data.users;
+
+        let list = document.getElementById('app');
+        list.innerHTML = "";
+
+        users.forEach(user => {
+            const el = document.createElement('p');
+            el.textContent = `${user.firstName} ${user.lastName} - ${user.email}`;
+            list.append(el);
+            console.log(user.firstName);
+        });
+    })
+    .catch(error => {
+        console.log("Ошибка: ", error);
+        document.getElementById('app').textContent = "Ошибка загрузки!";
+    })
