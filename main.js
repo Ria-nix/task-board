046cb5f54f3613c0e35ce8dbcf16b2ad480b4a38
RiaNix <podkovadasha@gmail.com>
2026-01-19 23:02:05 +0200

    table view

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-const url = "https://dummyjson.com/users?limit=10";
+const url = "https://dummyjson.com/users?limit=60";
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        let users = data.users;
+  .then((response) => response.json())
+  .then((data) => {
+    let users = data.users;
+    let table = document.querySelector(".app-list");
 
-        let list = document.getElementById('app');
-        list.innerHTML = "";
+    users.forEach((user) => {
+      let row = document.createElement("tr");
 
-        users.forEach(user => {
-            const el = document.createElement('p');
-            el.textContent = `${user.id}: ${user.firstName} ${user.lastName} - ${user.email}`;
-            list.append(el);
-            console.log(user.firstName);
-        });
-    })
-    .catch(error => {
-        console.log("Ошибка: ", error);
-        document.getElementById('app').textContent = "Ошибка загрузки!";
-    })
+      row.innerHTML = `<td>${user.id}</td>
+                        <td>${user.firstName}</td>
+                        <td>${user.lastName}</td>
+                        <td>${user.maidenName || "-"}</td>
+                        <td>${user.gender}</td>
+                        <td>${user.age}</td>
+                        <td>${user.email}</td>
+                        <td>${user.phone}</td>`;
+
+      table.appendChild(row);
+    });
+  })
+  .catch((error) => {
+    console.log("Ошибка: ", error);
+    document.getElementById("app").textContent = "Ошибка загрузки!";
+  });
